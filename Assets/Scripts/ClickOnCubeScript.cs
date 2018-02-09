c5238b9fc1b69538a80a92ec881b440dae6edb2a
E. Ung <evee.ung@gmail.com>
2018-02-09 16:13:09 -0500

    Added parent's dialogue in SafeSpace scene.

---
+++ Assets/Scripts/ClickOnCubeScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOnCubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check if mouse button is down
+		if (Input.GetMouseButtonDown (0)) {
+			//create a ray from us through the direction the camera is facing
+			Ray ray = new Ray (transform.position, Camera.main.transform.forward);
+			RaycastHit hit;
+
+			//trigger the sound on the cube if the ray hits it
+			if (Physics.Raycast (ray, out hit)) {
+				if (hit.collider.gameObject.tag == "Audio Cube") {
+					hit.collider.gameObject.GetComponent<AudioSource> ().Play ();
+				}
+			}
+		}
+	}
+			
+}
---
+++ Assets/TriggerAudioScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudioScript : MonoBehaviour {
+
+	public AudioSource mySource;
+	bool hasPlayed;
+
+	// Use this for initialization
+	void Start () {
+		
+		hasPlayed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			if (!mySource.isPlaying) {
+				mySource.Play ();
+				hasPlayed = true;
+			}
+		}
+	}
+}
