86ed09d355551d44dbc40a9cdd910c92846d5df5
Mannie <manshook@gmail.com>
2018-02-11 22:59:22 -0500

    add good whispers particle movement script

---
+++ Assets/ParticleMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleMovement : MonoBehaviour {
+
+	public float moveSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate( 0,-moveSpeed*Time.deltaTime , 0);
+
+		
+	}
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.name == "Arm") {
+			print ("catch!");
+			Destroy (gameObject);
+			GameManager.instance.health++;
+		}
+//		} else {
+//			Destroy (gameObject);
+//		}
+	}
+}
