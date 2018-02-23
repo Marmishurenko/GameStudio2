731baf4791380cc999cbb703513eebc1a8bafc5e
Mannie <manshook@gmail.com>
2018-02-23 11:30:18 -0500

    add trash collection mechanic, timer, healthscore

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
 
 public class GameManager : MonoBehaviour {
 
 	public static GameManager instance = null;
 
-	public int health = 10;
+
+	public int health = 0;
+	public float time = 0;
+	public int bestHealth;
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -15,15 +23,44 @@ public class GameManager : MonoBehaviour {
 			DontDestroyOnLoad(gameObject);
 		}
 		else {
-			instance.health = 10;
-
+			instance.health = 0;
 			Destroy(gameObject); // THERE CAN BE ONLY ONE!
 		}
+		time = Time.time;
+
+	
+		if (PlayerPrefs.HasKey("bestHealth")) {
+			bestHealth = PlayerPrefs.GetInt("bestHealth");
+		}
+
 
 	}
 
 	// Update is called once per frame
 	void Update () {
+		
+		time++;
+		if (time == 100) {
+			EndGame ();
+		}
 
 	}
+
+	public void EndGame(){
+		 
+		if (health > bestHealth) {
+				bestHealth = health;
+
+			PlayerPrefs.SetInt ("bestHealth",bestHealth);
+				PlayerPrefs.Save ();
+			}
+		time = 0f;
+		SceneManager.LoadScene("SafeSpace");
+
+
+
+	}
+
+
+
 }
---
+++ Assets/UpdateHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UpdateHealth : MonoBehaviour {
+
+	Text myText;
+
+
+	// Use this for initialization
+	void Start () {
+		myText = gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myText.text = "Health " + GameManager.instance.health + "Best Health " + GameManager.instance.bestHealth + " Time: " + GameManager.instance.time.ToString() ;
+		
+	}
+
+
+}
