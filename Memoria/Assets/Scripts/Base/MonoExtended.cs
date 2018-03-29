45fb69a7909bc827e4d698c4dadab644c7b103c4
Xiaoxun Yu <xunersmail@gmail.com>
2018-03-29 17:05:14 -0400

    Basic folder structure Added Game Manager Added MonoExtended and a sample class

---
+++ Memoria/Assets/Scripts/Base/MonoExtended.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Extended MonoBehaviour class. Should be used instead
+// Check MonoExtendExample class for example
+public abstract class MonoExtended : MonoBehaviour {
+
+    protected GameManager gameManager;    // Every MonoExtended class has direct access to GamaManager
+
+    protected void Awake() {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    void Update() {
+        // Game state like running or paused
+        switch (gameManager.gameState) {
+            case GAME_STATE.RUNNING:
+                GameUpdate();
+                break;
+
+            case GAME_STATE.PAUSED:
+                break;
+        }
+    }
+
+    protected abstract void GameUpdate();
+}
---
+++ Memoria/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// General game manager which exists through the whole life cycle of the game
+public class GameManager : MonoBehaviour {
+
+    // TODO tooltip
+    // TODO Text mesh pro
+    public GAME_STATE gameState;    // Controls the general state of the game, like running, paused, etc.
+
+    void Start() {
+        DontDestroyOnLoad(gameObject);
+        gameState = GAME_STATE.RUNNING;
+    }
+
+    void Update() {
+
+    }
+}
+
+
+public enum GAME_STATE { RUNNING, PAUSED };
