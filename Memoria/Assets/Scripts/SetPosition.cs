60ec43a7ecaf5a5710e46b43ab3c78b43ca1537c
Mannie <manshook@gmail.com>
2018-04-02 13:24:36 -0400

    add mouse dragging mechanic

---
+++ Memoria/Assets/Scripts/SetPosition.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPosition : MonoBehaviour {
+
+	public void setPosition(Vector3 pos){
+		transform.position = pos;
+	}
+}
