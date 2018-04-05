859d87c350b92467c539009e924c545b65887339
Xiaoxun Yu <xunersmail@gmail.com>
2018-04-05 16:43:41 -0400

    Moved all scripts to the Scripts folder

--- Memoria/Assets/Scripts/Texting/MessageWindow.cs
+++ Memoria/Assets/Scripts/Texting/MessageWindow.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MessageList : MonoBehaviour {
+public class MessageWindow : MonoBehaviour {
 
     float targetPos;
 
---
+++ Memoria/Assets/Sprites/SpongeControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using TMPro;
+
+public class SpongeControl : MonoBehaviour {
+    string curTag;
+    bool isAunt{ get { return curTag == "Aunt" ? true :false; }}
+    public UnityEvent OnAuntEnter;
+    public UnityEvent OnAuntExit;
+    public UnityEvent OnAuntRub;
+    public UnityEvent OnBucketEnter;
+    public UnityEvent OnBucketExit;
+
+
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Rub(){
+        if(isAunt){
+            OnAuntRub.Invoke();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+	{
+        curTag = other.tag;
+        print("enter");
+        if (other.tag == "Aunt"){
+            OnAuntEnter.Invoke();
+        }
+        if (other.tag == "Bucket")
+        {
+            OnBucketEnter.Invoke();
+        }
+	}
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        curTag = "";
+        print("exit");
+        if (other.tag == "Aunt")
+        {
+            OnAuntExit.Invoke();
+        }
+        if (other.tag == "Bucket")
+        {
+            OnBucketExit.Invoke();
+        }
+    }
+}
