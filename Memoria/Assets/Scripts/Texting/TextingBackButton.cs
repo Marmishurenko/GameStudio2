10f23643da60059de8030143f557d6e6071925cd
Xiaoxun Yu <xunersmail@gmail.com>
2018-04-19 17:28:36 -0400

    Fixed cursor bug Added finger cursor

--- Memoria/Assets/Scripts/Texting/TextingBackButton.cs
+++ Memoria/Assets/Scripts/Texting/TextingBackButton.cs
@@ -17,9 +17,9 @@ public class TextingBackButton : MonoExtended {
 
         timer -= Time.deltaTime;
         float period = 2.5f;
-        Color c = gameObject.GetComponent<Image>().color;
+        Color c = gameObject.GetComponent<SpriteRenderer>().color;
         c.a = (Mathf.Sin((period - timer) / period * Mathf.PI * 2) + 1) * 0.5f * 0.8f + 0.2f;
-        gameObject.GetComponent<Image>().color = c;
+        gameObject.GetComponent<SpriteRenderer>().color = c;
 
         if (timer <= 0)
             timer = period;
