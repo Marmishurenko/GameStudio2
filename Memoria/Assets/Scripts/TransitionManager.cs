ddcafeb075f291e7bd6eadb2659425cbe7856d95
Xiaoxun Yu <xunersmail@gmail.com>
2018-04-24 16:12:15 -0400

    Added text to transition scenes Multiple bugs fixed

--- Memoria/Assets/Scripts/TransitionManager.cs
+++ Memoria/Assets/Scripts/TransitionManager.cs
@@ -1,20 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TransitionManager : MonoExtended {
 
-    [SerializeField] float DURATION;
-
-    float timer;
+    [SerializeField] List<string> textList;
+    [SerializeField] TextMeshProUGUI textUI;
 
     void Start() {
-        timer = DURATION;
+        StartCoroutine("Transition");
     }
 
     protected override void GameUpdate() {
-        timer -= Time.deltaTime;
-        if (timer <= 0)
+    }
+
+    IEnumerator Transition() {
+        string s = textList[gameManager.gameStage].Replace("|", "\n");
+        textUI.SetText(s);
+        yield return new WaitForSeconds(0.5f);
+
+        // No text to show
+        if (textUI.text.Length == 0) {
+            yield return new WaitForSeconds(0.5f);
             gameManager.LoadNextGameScene();
+            yield break;
+        }
+
+        // Show text
+        while (Input.GetMouseButton(0) == false) {
+            yield return null;
+        }
+        yield return new WaitForSeconds(1);
+        gameManager.LoadNextGameScene();
     }
 }
