e3822e9abc5011cc0ba36b134ea183476c7ee888
Mannie <manshook@gmail.com>
2018-05-07 19:09:18 -0400

    fixed trigger sounds in cleaning cycle one

---
+++ Memoria/Assets/BinControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BinControl : MonoBehaviour {
+
+    public UnityEvent OnBasketEnterEvent;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D (Collider2D other)
+	{
+        OnBasketEnterEvent.Invoke();
+	}
+}
