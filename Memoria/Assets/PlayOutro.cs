7a4b101a7ec7cc87343e50180933d7ab624cea14
Mannie <manshook@gmail.com>
2018-05-07 21:21:36 -0400

    added intro

--- Memoria/Assets/PlayOutro.cs
+++ Memoria/Assets/PlayOutro.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayOutro : MonoBehaviour {
-
+    private AudioSource aus;
 	// Use this for initialization
 	void Start () {
-		
+        aus = gameObject.GetComponent<AudioSource>();
+        aus.Play();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+      
 		
 	}
 }
